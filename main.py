c2e6680125f63476cef39344f98852e740be77b8
Icy Cloud <i2cy@outlook.com>
2022-06-02 00:38:28 +0800

    renamed img_scan_beta to scanner.py, added main.py for entrance

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: i2cy(i2cy@outlook.com)
+# Project: sources
+# Filename: main
+# Created on: 2022/6/2
+
+from api.htsocket import PTControl
+from api.scanner import Scanner
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
