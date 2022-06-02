eab3dbb1e3ec3f84423aa4d9c96169f5f54f253d
Icy Cloud <i2cy@outlook.com>
2022-06-02 17:48:01 +0800

    updated control.py

--- api/control.py
+++ api/control.py
@@ -14,6 +14,30 @@ else:
     from .htsocket import PTControl
 
 
+class LaserYawControl(PID):
+
+    def __init__(self, ctl_clt, kp=1, ki=0, kd=0, core_freq=50):
+        super(LaserYawControl, self).__init__(kp=kp, ki=ki, kd=kd, core_freq=core_freq)
+        assert isinstance(ctl_clt, PTControl)
+
+        self.ctl_clt = ctl_clt
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.dt = 1 / core_freq
+
+        self.last_out = 0
+
+    def coreTask(self):
+        self.ctl_clt.speed_pitch = self.out - self.last_out
+        self.last_out = self.out
+
+    def start(self):
+        if not self.ctl_clt.live:
+            self.ctl_clt.connect()
+        super(LaserYawControl, self).start()
+
+
 class LaserPitchControl(PID):
 
     def __init__(self, ctl_clt, kp=1, ki=0, kd=0, core_freq=50):
